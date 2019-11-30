a322c536a995bacf4e235c8941b09376956e0f0c
GSeckin <49096929+GSeckin@users.noreply.github.com>
2019-11-30 21:54:42 +0200

    new branch 30.11
    new branch, "Blog" değişti

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Gonderi
+from .forms import GonderiForm
 
 def gonderi_liste(request):
     Gonderiler = Gonderi.objects.all()
@@ -9,3 +10,6 @@ def gonderi_detay(request,pk):
     yazi = get_object_or_404(Gonderi,pk=pk)
     return render(request, 'GorkemBlog/gonderi_detay.html', {'gonderi':yazi})
     
+def yeni_gonderi(request):
+    Form = GonderiForm()
+    return render(request, 'GorkemBlog/gonderi_duzenle.html', {'form': Form})
